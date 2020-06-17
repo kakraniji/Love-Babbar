e8a13974d6589006a6656ffe1da164e53f832977
kakraniji <60791683+kakraniji@users.noreply.github.com>
2020-06-17 19:19:17 +0530

    Update Exchange First And Last Nodes In Circular Linked List .cpp

--- Linked List/Exchange First And Last Nodes In Circular Linked List .cpp
+++ Linked List/Exchange First And Last Nodes In Circular Linked List .cpp
@@ -1,6 +1,8 @@
 
 //yaha par last second node se khelna padta he !!
 
+//pele first node ko uthakar last second node se point karwao or jo last node padi he use first se point karwado !!!
+
 while(p->next->next!=head)
 p=p->next;
 p->next->next=head->next;
